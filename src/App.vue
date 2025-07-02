33f322f16d8137032caf598e5a4afa96371d22fd
eagle4246 <eagle4246@hotmail.com>
2025-07-02 09:50:57 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Message from './auo/ui/Message.vue';
+</script>
+
+<template>
+  <h1>AUO Day 3</h1>
+  <Message user="Bob" age="121234" />
+</template>
+
+<style scoped></style>
---
+++ src/auo/ui/Message.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+defineProps({
+  user: String,
+  age: Number,
+});
+let count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div>Hello {{ user }}, {{ age }}</div>
+  <button @click="decrement">-</button>
+  count{{ count }}
+  <button @click="increment">+</button>
+</template>
+<style scoped>
+div {
+  font-weight: bold;
+}
+</style>
